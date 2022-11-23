ff424df9ea928f8de4f0b8a6ea70f3ccb773ccbd
Floren henri <>
2022-11-23 21:42:04 +0100

    Init the project

--- front/src/lib/formatDate.js
+++
@@ -1,8 +0,0 @@
-export function formatDate(dateString) {
-  return new Date(`${dateString}`).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-    timeZone: 'UTC',
-  })
-}
